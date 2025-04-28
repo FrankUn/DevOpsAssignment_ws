643784bf4b88ef0d420fe1f6a88c5dd0774e14c6
Frank UbuntuDesktop <uniekun1@gmail.com>
2025-04-28 19:10:19 +0800

    fix the termination logic

--- scripts/start_simulation.sh
+++ scripts/start_simulation.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+FAIL_CRITERIA=0.5
 
 # Source ROS setup
 echo "Source ROS setup"
@@ -7,6 +8,8 @@ source /opt/ros/noetic/setup.bash
 # Source workspace setup
 echo "Source workspace setup"
 source /home/bot/workspace/my_first_build_pipeline/assignment_ws/devel/setup.bash
+
+echo "The fail criteria is set to be crosstrack error greater than $FAIL_CRITERIA meter"
 
 # Start the simulation
 rosrun gem_pure_pursuit_sim pure_pursuit_sim.py | while read -r line
@@ -22,10 +25,11 @@ do
 
         echo "Absolute Crosstrack Error: $abs_crosstrack_error"
 
-        # Check if the absolute Crosstrack Error is greater than 1 meter using awk
-        result=$(echo "$abs_crosstrack_error > 0.5" | awk '{if ($1 == 1) print 1; else print 0}')
+        # Check if the absolute Crosstrack Error is greater than the failure threshold using awk
+        result=$(echo "$abs_crosstrack_error $FAIL_CRITERIA" | awk '{if ($1 > $2) print 1; else print 0}')
+        
         if [ "$result" -eq 1 ]; then
-            echo "Absolute Crosstrack Error exceeds 1m, terminating simulation!"
+            echo "Absolute Crosstrack Error exceeds $FAIL_CRITERIA m, terminating simulation!"
             # Terminate the simulation by killing the rosrun process
             pkill -f "rosrun gem_pure_pursuit_sim pure_pursuit_sim.py"
             exit 1
